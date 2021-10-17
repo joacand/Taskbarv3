2a5797d07e7d6337e274524d8604f3e0ac9a5592
Joacim Andersson <joacand@users.noreply.github.com>
2021-10-17 15:42:32 +0200

    Change configuration files to use %appdata% (#3)

---
+++ Src/Taskbarv3.Infrastructure/DataAccess/DataHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Taskbarv3.Infrastructure.DataAccess
+{
+    public abstract class DataHandler
+    {
+        private static string ApplicationName => "Taskbarv3";
+
+        protected static string ApplicationSettingsDirectory { get; } =
+            Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ApplicationName);
+
+        protected void CreateDirectory()
+        {
+            Directory.CreateDirectory(ApplicationSettingsDirectory);
+        }
+    }
+}
