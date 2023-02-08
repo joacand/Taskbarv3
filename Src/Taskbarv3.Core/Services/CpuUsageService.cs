daf33a78a084bb0a12b0ff759f9ddcf09645cb04
Joacim Andersson <joacand@users.noreply.github.com>
2023-02-08 17:26:18 +0100

    Update framework (#8)
    In addition:
    * Possibility to disable CPU monitor in config

--- Src/Taskbarv3.Core/Services/CpuUsageService.cs
+++ Src/Taskbarv3.Core/Services/CpuUsageService.cs
@@ -2,34 +2,33 @@
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Taskbarv3.Core.Interfaces;
+using Taskbarv3.Core.Models;
 
 namespace Taskbarv3.Core.Services
 {
     public sealed class CpuUsageService : ICpuUsageService, IDisposable
     {
+        private readonly MainConfig config;
         private PerformanceCounter cpuCounter;
         private bool initialized;
 
-        public CpuUsageService()
+        public CpuUsageService(IConfigHandler configHandler)
         {
-            // PerformanceCounter can have a slow start-up time - start in a separate thread
-            Task.Run(() =>
-            {
-                cpuCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
-                cpuCounter.NextValue();
-                initialized = true;
-            });
-        }
+            config = configHandler.LoadFromFile();
 
-        public int NextValue {
-            get {
-                if (!initialized)
+            if (!config.CpuMonitorDisabled)
+            {
+                // PerformanceCounter can have a slow start-up time - start in a separate thread
+                Task.Run(() =>
                 {
-                    return 0;
-                }
-                return (int)Math.Round(cpuCounter.NextValue());
+                    cpuCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+                    cpuCounter.NextValue();
+                    initialized = true;
+                });
             }
         }
+
+        public int NextValue => !initialized ? 0 : (int)Math.Round(cpuCounter.NextValue());
 
         public void Dispose()
         {
