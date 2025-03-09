47e3eb21e0697b7857261c7d884f4bff1973e6c8
joacand <joacand@users.noreply.github.com>
2025-03-09 09:08:20 +0100

    Improve the UI rendering (#12)

--- Src/Taskbarv3.UI/Views/AddShortcutView.xaml.cs
+++ Src/Taskbarv3.UI/Views/AddShortcutView.xaml.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Taskbarv3.UI.Views
 {
-    /// <summary>
-    /// Interaction logic for AddShortcutView.xaml
-    /// </summary>
     public partial class AddShortcutView : Window
     {
         public AddShortcutView()
         {
             InitializeComponent();
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
