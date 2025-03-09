6297d53459c176e2eb4c6745a31fba55afdbfda2
joacand <tbsitu@gmail.com>
2025-03-09 09:07:30 +0100

    Improve the UI rendering

--- Src/Taskbarv3.UI/Views/SettingsView.xaml.cs
+++ Src/Taskbarv3.UI/Views/SettingsView.xaml.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Taskbarv3.UI.Views
 {
-    /// <summary>
-    /// Interaction logic for SettingsView.xaml
-    /// </summary>
     public partial class SettingsView : Window
     {
         public SettingsView()
         {
             InitializeComponent();
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
